fe8fed56f0b643d9f1d77da34bb18a82a54ef9ff
Nishant2356 <nm2356opp@gmail.com>
2026-02-03 00:24:07 +0530

    fixed-auth

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,25 +1,26 @@
 import NextAuth, { DefaultSession } from "next-auth";
 import { JWT } from "next-auth/jwt";
+import { Role, Department } from "@prisma/client";
 
 declare module "next-auth" {
     interface Session {
         user: {
             id: string;
-            role: string;
-            domain: string;
+            role: Role;
+            domain: Department[];
         } & DefaultSession["user"];
     }
 
     interface User {
-        role: string;
-        domain: string;
+        role: Role;
+        domain: Department[];
     }
 }
 
 declare module "next-auth/jwt" {
     interface JWT {
-        role: string;
-        domain: string;
+        role: Role;
+        domain: Department[];
         id: string;
     }
 }
