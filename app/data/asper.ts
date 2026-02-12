8ff2f793fe78bbfc497b936372aecc90c2db4936
Nishant2356 <nm2356opp@gmail.com>
2026-02-13 01:18:25 +0530

    asper_data

--- app/data/asper.ts
+++ app/data/asper.ts
@@ -3,5 +3,5 @@ export const asper = {
     email: "connect.asperofficial@gmail.com",
     linkedin: "https://www.linkedin.com/company/theasperteam/?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAdGRzdgP7BzZleHRuA2FlbQIxMQBzcnRjBmFwcF9pZA81NjcwNjczNDMzNTI0MjcAAaciA5onjthiJL10wyj1ssIhclM68xa2IPzkwkX0MGxvFrZENt7ks7PDAiwKlQ_aem_YXll6illXpXKgg_AWNU2nA",
     instagram: "https://www.instagram.com/asper.uitrgpv?igsh=cWdyMWVudzNpNjV1",
-    github: "https://github.com/asper-uitrgpv"
+    github: "https://github.com/Asper-GH"
 };
